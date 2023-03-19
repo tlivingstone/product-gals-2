743652b98ab7c5d54cbc4507fee13aa07059379a
Taryn Livingstone <livingstonetaryn@gmail.com>
2023-03-19 13:32:20 -0700

    styling

---
+++ src/app/data/Event.ts
@@ -0,0 +1,9 @@
+const events = [
+    {
+        name: "Product Women Mixer at Arcana 🪴",
+        description: "We hosted a wine & new friends evening in Arcana's beautiful urban jungle!",
+        date: "14 March 2023"
+    }
+];
+
+export default events;
---
+++ src/components/Event.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Event(props: {
+    name: string;
+    description: string;
+    date: string;
+}) {
+    return (
+        <>
+        <div className = "">
+            <div className = "">
+                    <p className ="font-light text-md pb-2 text-gray-500 tracking-widest font-mono">{props.date}</p>
+                    <p className= "text-3xl font-mono pb-8">{props.name}</p>
+                    <p className = "tracking-widest font-light text-xl w-1/2 leading-8">{props.description}</p>
+                    <p className = "pt-3 font-light tracking-widest text-lg w-1/2 leading-8">Thank you <a className = "underline decoration-pink-500 font-normal" href="https://www.firstrepublic.com/">First Republic&#8599;</a> and <a  className = "underline decoration-sky-500 font-normal" href="https://www.thisisartium.com/">Artium&#8599;</a> for sponsoring!</p>
+
+            </div>
+            
+        </div>
+      </>
+    )
+}
